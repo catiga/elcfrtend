3a2fe2aba98458e3ba8dc991b130d06ff687b309
biangz <nnuauroral@163.com>
2022-03-12 23:08:13 +0800

    添加页面

---
+++ src/renderer/views/ProjectMgr/risk.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>风险评估</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+<style type="text/css" lang="scss" scoped>
+
+</style>
---
+++ src/renderer/views/ProjectMgr/weak.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>薄弱环节分析</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+<style type="text/css" lang="scss" scoped>
+
+</style>
