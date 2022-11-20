df7bedba74c84f148089651f0eabde0756e0b509
jackie lee <catiga03@gmail.com>
2022-11-20 12:51:28 +0800

    1

--- midware/syssql.js
+++ midware/syssql.js
@@ -3,6 +3,8 @@
 import fs from 'fs';
 import path from 'path'
 
+const log = require('electron-log');
+
 // export const readFile = function(callback) {
 //     fs.readFile(path.join(__dirname, './wh_smartgrid.sql'), (err, data) => {
 //         if(err) callback(err)
@@ -13,6 +15,8 @@ import path from 'path'
 // export async function readFile2() {
 export const readFile = async function() {
     console.log('here called success in')
+    log.info('Hello, log');
+    log.warn('Some problem appears');
     return new Promise((resolve, reject) => {
         let sqlFilePath;
         if (process.env.NODE_ENV === 'development') {
@@ -20,6 +24,8 @@ export const readFile = async function() {
         } else {
             sqlFilePath = path.join(process.cwd(), '/resources/midware/wh_smartgrid.sql')
         }
+        console.log('normal:', sqlFilePath)
+        log.info('eclog:', sqlFilePath)
         // fs.readFile(path.join(__dirname, './wh_smartgrid.sql'), (err, data) => {
             fs.readFile(sqlFilePath, (err, data) => {
             if(err) reject(err)
